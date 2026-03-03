814f273b253e85c49df992df9825766f565e0d74
Script Services <scriptservices11@gmail.com>
2026-03-03 13:05:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8018207470-dfd76",
+  "appId": "1:349303372179:web:9f94e44c7baac53b911066",
+  "apiKey": "AIzaSyBCMoqtNhafR4R9CILJJy8KqJBYFajPCT0",
+  "authDomain": "studio-8018207470-dfd76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "349303372179"
+};
